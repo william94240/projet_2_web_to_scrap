d618c70d99eff01a34e03a4919226b6c7ef05beb
william94240 <william.mopete.k@live.fr>
2023-11-03 16:33:40 +0100

    ajout de la fonction de chargement sur fichiers csv

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ import csv
 
 
 
-
 #Parcourir toutes les pages du site web
 def parcourir_toutes_les_pages():
     pages_urls = []
@@ -39,84 +38,73 @@ def parcourir_toutes_les_pages():
 
 
 
-
-#Récupérer l'adresse de chaque livre (product_page_url)
+# Récupérer l'adresse de chaque livre_product_page_url
 def recuperer_ladresse_dulivre(page_url):
     response = requests.get(page_url)
-    code_source_delapage = response.content
-    soup = BeautifulSoup(code_source_delapage, "html.parser")
+    html_delapage = response.content
+    soup = BeautifulSoup(html_delapage, "html.parser")
     livres_surlapage = soup.find_all("article", class_="product_pod")
  
 
     product_page_urls = []
     for livre in livres_surlapage:      
         #Obtenir Les titres. 
-        link = livre.find("a")["href"]
+        link = livre.find("a").attrs["href"]
         #Obtenir L'url de la page spécifique du livre.
         product_page_url = "http://books.toscrape.com/catalogue/" + link
         product_page_urls.append(product_page_url)
    
     return product_page_urls
 
-
-
-
-        
-
+    
+     
 # Otenir des infos sur chaque livre
-def infos_chaque_livre(product_page_urls):
+def infos_chaque_livre():
     product_page_urls = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
     response = requests.get(product_page_urls)
-    codesource_pagelivre =  response.content                                                                                                                       
-    soup = BeautifulSoup(codesource_pagelivre, "html.parser")
+    html_pagelivre =  response.content                                                                                                                       
+    soup = BeautifulSoup(html_pagelivre, "html.parser")
 
     # Le contenu de la page entière d'un seul article(livre)
     page_livrecontenu = soup.find("article", class_="product_page")
-    
-    title = page_livrecontenu.find("h1").string
-    etoiles = page_livrecontenu.find("p", class_="star-rating Three").attrs["class"][1]
+    title = page_livrecontenu.find("h1").string         
+       
+    tableau_infos_article = soup.find("table")
+    liste_des_lignes = tableau_infos_article.find_all("tr")
+    for ligne in liste_des_lignes:
+        upc = liste_des_lignes[0].td.string
+        product_type = liste_des_lignes[1].td.string
+        price_excl_tax = liste_des_lignes[2].td.string
+        price_incl_tax = liste_des_lignes[3].td.string
+        tax = liste_des_lignes[4].td.string
+        availability = liste_des_lignes[5].td.string
+        number_of_reviews = liste_des_lignes[6].td.string
+
     product_description_titre = page_livrecontenu.find(id="product_description")
     product_description = product_description_titre.find_next().find_next().text
 
     barre_denavigation = soup.find("ul", class_="breadcrumb")
     category =barre_denavigation.find_all("li")[2].find("a").text
 
-    image_url = soup.find("div", class_="item active").img["src"]
+    image_url = soup.find("div", class_="item active").img.attrs["src"]
     
-       
-    tableau = soup.find("table")
-    lignes_dutableau = tableau.find_all("tr")
-    for ligne in lignes_dutableau:
-        upc = lignes_dutableau[0].td.string
-        product_type = lignes_dutableau[1].td.string
-        price_excl_tax = lignes_dutableau[2].td.string
-        price_incl_tax = lignes_dutableau[3].td.string
-        tax = lignes_dutableau[4].td.string
-        availability = lignes_dutableau[5].td.string
-        number_of_reviews = lignes_dutableau[6].td.string
-
-       
-
-        
-        
-    print("upc: ", upc)    
-    print("title: ", title)    
-    print("price_excl_tax: ", price_excl_tax)    
-    print("price_incl_tax: ", price_incl_tax)    
-    print("availability: ", availability)    
-    print("product_description: ", product_description)    
-    print("category: ", category)    
-    print("number_of_reviews: ", number_of_reviews)    
-    print("image_url: ", image_url)    
-    
+    etoiles = page_livrecontenu.find("p", class_="star-rating Three").attrs["class"][1]
+   
   
-    return upc, title, price_excl_tax, price_incl_tax, availability, product_description, category, number_of_reviews, image_url
+       
+    infos_livres =[upc, title, price_excl_tax, price_incl_tax, availability, category, number_of_reviews, image_url, etoiles, product_description]
+ 
+    return infos_livres
 
-      
-page_url = "http://books.toscrape.com/index.html"
 
-parcourir_toutes_les_pages()
-recuperer_ladresse_dulivre(page_url)
+
+# Ecriture dans un fichier csv    
+def ecrire_dans_csv(infos_livres):
+    with open("Book to scrape.cvs", "w",newline="", encoding="utf8") as file:
+        label_colonnes =["UPC", "TITRE", "PRICE_EXCL_TAX", "PRICE_INCL_TAX", "AVAILABILITY", "CATEGORY", "NUMBER_OF_REVIEWS", "IMAGE_URL", "ETOILES", "PRODUCT_DESCRIPTION"]
+        writer = csv.writer(file)
+        writer.writerow(label_colonnes)
+        writer.writerow(infos_livres)
 
 
 
