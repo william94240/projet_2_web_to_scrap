ac31afc41565513c903093acdf5334daf63440f7
william94240 <william.mopete.k@live.fr>
2023-12-01 09:34:23 +0100

    Mise à jour du fichier README.md

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 #### Project 2 : Utilisez les bases de Python pour l'analyse de marché.
 
-+ Veuillez créer un environnement virtuel
-    1. d'abord:     ***pip install virtualenv*** .
-    2. et ensuite : ***python -m venv env***.
 
-+ Activer l'environnement virtuel:
-    * Pour le _Shell Bash_ à l'aide de la commande:   ***source env/script/activate***
-    * Pour le _terminal cmd_ à l'aide de la commande: ***.\env\script\activate.bat***
+## Description
 
-+ Veuillez installer les packages énumérés dans le fichier __requirement.txt__ à l'aide de la commande ***pip install -r requirements.txt***.
+Il s'agit d'un programme de scraping Web conçu pour extraire des informations du site Web "Books to scrape". Le but de ce programme est de collecter des données liées aux livres à des fins d'analyse ou à d'autres fins.
 
-+ L'appel de la fonction **scrapper_book_to_scrap()** déclenche automatiquement le scrapping du site **BOOK TO SCRAP**.
++ Rendez-vous dans le dossier contenant le script avec la commande ``cd``
+
++ Veuillez créer un environnement virtuel.
+
+    d'abord par l'installation virtualenv (si virtualenv n'est pas installé): ``pip install virtualenv`` .
+
+    ou directement  à l'aide de la commande ``python -m venv env.``
+
+
++ Activez l'environnement virtuel:
+    * Pour le __Shell Bash__ à l'aide de la commande:   ``source env/script/activate``
+    * Pour le __terminal windows__ à l'aide de la commande: ``.\env\script\activate.bat``
+
++ Veuillez installer les packages énumérés dans le fichier __requirement.txt__ à l'aide de la commande:  ``pip install -r requirements.txt``.
+
++ Lancez le programme avec python en ligne de commande avec l'instruction  " `` python main.py``". main est le script python principal. celui-ci déclenchera l'appel de la fonction «_scrapper_book_to_scrap()_» ,qui amorcera à son tour automatiquement le scrapping du site « Books to scrape ». Le programme commencera à récupérer les données du site Web « Books to scrape ».Le programme va générer des données de 2 types, reparties en 2 dossiers principaux _csv et images_. Les informations textuelles relatives à chaque livre sont regroupées en des catégories corresponds à des fichiers_.csv_ du dossier _csv_ dont les dénominations sont tirées du site web « Books to scrape ». Il en va de même pour les fichiers images, qui sont par contre enregistrés en binaire au format _.jpg_ et regroupés également en catégories qui correspondent à des sous-dossiers du dossier _images_.
+
++ Si vous rencontrez des problèmes ou avez des suggestions d'amélioration, veuilez m'en informé. 
+
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-''' 
-PROJET BOOK TO SCRAPE (WEB SCRAPPER)
-D'abord créer un environement virtuel  avec: python -m venv env
-Initialiser un repertory local git avec: git init
-Créer le fichier requirements: pip freeze >requirements.txt avec redirection sur le fichier requirements.txt
-Dans .gitignore exclure: les fichiers de l'environement virtuel, les fichiers CSV et images.
-'''
+# PROJET BOOK TO SCRAPE (WEB SCRAPPER)
+# D'abord créer un environement virtuel  avec: python -m venv env
+# Initialiser un repertory local git avec: git init
+# Créer le fichier requirements: pip freeze >requirements.txt avec redirection sur le fichier requirements.txt
+# Dans .gitignore exclure: les fichiers de l'environement virtuel, les fichiers CSV et images.
 
 
 # Avec PIP installer les packages néccessaires
@@ -30,7 +28,7 @@ def parcourir_toutes_les_categories():
         # Trouve la liste de catégories.
         liste_categories = soup.find(
             "ul", class_="nav nav-list").ul.find_all("li")
-        # Initialisation de la liste des urls de differentes catégories.
+        # Initialisation de la liste des urls de différentes catégories.
         liste_urls_categories = []
         for categorie in liste_categories:
             # trouve une partie de l'adresse.
@@ -44,7 +42,7 @@ def parcourir_toutes_les_categories():
             html_categorie = response_categorie.content
             soup_categorie = BeautifulSoup(html_categorie, "html.parser")
 
-            # Pour trouver ligne en bas de la page où est afficher la numérotaion de la page.
+            # Pour trouver la ligne en bas de la page, où est affichée la numérotaion de la page.
             pagination = soup_categorie.find("li", class_="current")
 
             if pagination == None:
@@ -66,7 +64,7 @@ def parcourir_toutes_les_categories():
 
 
 def scrapper_book_to_scrap():
-    # Permet de récupérer l'adresse de chaque livre :product_page_url.
+    # Permet de récupérer l'adresse de chaque livre.
     # SURTOUT, c'est la FONCTION PRINCIPALE qui permet de scrapper le site web.
 
     # la liste des urls des catégories.
@@ -82,8 +80,7 @@ def scrapper_book_to_scrap():
         liste_livres_par_page = soup.find_all("article", class_="product_pod")
 
         for livre in liste_livres_par_page:
-            # Parcourir tous les livres pour obtenir leurs urls.
-            # Obtenir l'url relative de chaque livre de la page.
+            # Parcourir tous les livres pour obtenir leurs urls relatives.
             link = livre.find("a").attrs["href"]
             # Obtenir L'url absolue du livre.
             url_product_page = urljoin(url_categorie, link)
@@ -99,32 +96,33 @@ def infos_livre(url_product_page):
     # Cette fonction est appelée par scrapper_book_to_scrap()
 
     # Parser chaque livre.
-    response = requests.get(url_product_page)  # Requête html sur le livre.
-    html_product_page = response.content  # html de la requête
-    soup = BeautifulSoup(html_product_page, "html.parser")  # L'objet soup
+    response = requests.get(url_product_page)
+    html_product_page = response.content
+    soup = BeautifulSoup(html_product_page, "html.parser")
 
     title = soup.h1.string   # Extraction du titre livre.
     # Trouve le tableau d'informations du livre.
     tableau_infos_article = soup.table
     # Extraction de chaque ligne du tableau d'infos.
     infos = tableau_infos_article.find_all("tr")
-    upc = infos[0].td.string  # info upc
-    product_type = infos[1].td.string  # info product_type
-    price_excl_tax = infos[2].td.string  # info price_excl_tax
-    price_incl_tax = infos[3].td.string  # info price_incl_tax
-    tax = infos[4].td.string  # info tax
-    availability = infos[5].td.string  # info availability
-    number_of_reviews = infos[6].td.string  # info number_of_reviews
+    upc = infos[0].td.string
+    product_type = infos[1].td.string
+    price_excl_tax = infos[2].td.string
+    price_incl_tax = infos[3].td.string
+    tax = infos[4].td.string
+    availability = infos[5].td.string
+    number_of_reviews = infos[6].td.string
     # Affiche la description du livre.
     product_description = soup.h2.find_next("p").text
 
-    # Isoler la barre de navigation en dessus horizontale
+    # Isole la barre de navigation en dessus horizontale
     # et trouve le nom de la catégorie.
     barre_denavigation = soup.find("ul", class_="breadcrumb")
     category = barre_denavigation.find_all("li")[-2].a.text
 
     # Trouve le url de l'image
-    url_image_relative = soup.img.attrs["src"]
+    url_image_relative = soup.img.attrs["src"]  # Adresse relative de l'image
+    # Adresse absolue de l'image
     url_image = urljoin(url_product_page, url_image_relative)
 
     # Extraction du nombre d'étoiles.
@@ -133,7 +131,7 @@ def infos_livre(url_product_page):
     # Extraction du nom qui sera attribué à l'image du livre lors du stockage des images.
     nom_image = url_product_page.split("/")[-2]
 
-    # liste des éléments non demandé à titre d'infos
+    # liste des éléments non demandés à titre d'infos
     infos_livres = [upc, title, price_excl_tax, price_incl_tax, availability, category,
                     number_of_reviews, url_image, etoiles, product_description]
 
@@ -159,11 +157,12 @@ def ecrire_dans_csv(category, infos_livres):
             writer = csv.writer(file, delimiter=",")
             # Ecririture de l'en-tête de chaque fichier csv.
             writer.writerow(label_colonnes)
+            # Ecririture des infos dans chaque fichier csv.
             writer.writerow(infos_livres)
     else:
         with open(filename, "a", newline="", encoding="utf8") as file:
             writer = csv.writer(file, delimiter=",")
-            # Inscription des références de chaque livre dans le fichier csv.
+            # Inscription des infos de chaque livre dans le fichier csv.
             writer.writerow(infos_livres)
 
 
